1ebadf5592c11c8f941a02e95fe22833a3f53c84
João Filipe Costa Coelho <jfilipecostacoelho@gmail.com>
2026-02-28 10:11:57 +0000

    UseState

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,40 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import { MenuLink } from '../MenuLink';
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement>) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <div className={styles.menu}>
-      <MenuLink>
+      <MenuLink link='#' label='Home'>
         <HouseIcon />
       </MenuLink>
 
-      <MenuLink>
+      <MenuLink link='#' label='History'>
         <HistoryIcon />
       </MenuLink>
 
-      <MenuLink>
+      <MenuLink link='#' label='Settings'>
         <SettingsIcon />
       </MenuLink>
 
-      <MenuLink>
+      <MenuLink link='#' label='Theme' onClick={handleThemeChange}>
         <SunIcon />
       </MenuLink>
     </div>
--- src/components/MenuLink/index.tsx
+++ src/components/MenuLink/index.tsx
@@ -2,11 +2,20 @@ import styles from './styles.module.css';
 
 type MenuLinkProps = {
   children: React.ReactNode;
+  link: string;
+  label: string;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
 };
 
-export function MenuLink({ children }: MenuLinkProps) {
+export function MenuLink({ children, link, label, onClick }: MenuLinkProps) {
   return (
-    <a className={styles.menuLink} href='#'>
+    <a
+      className={styles.menuLink}
+      href={link}
+      aria-label={label}
+      title={label}
+      onClick={onClick}
+    >
       {children}
     </a>
   );
